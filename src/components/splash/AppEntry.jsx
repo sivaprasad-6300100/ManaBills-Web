2c2d7bff0e550a77ac3bdc70584693c67151ecbd
sivap1015844 <sivap1015844@gmail.com>
2026-07-01 15:27:26 +0530

    fixing

--- src/components/splash/AppEntry.jsx
+++ src/components/splash/AppEntry.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
+// AppEntry.jsx
 import { Navigate } from "react-router-dom";
-import SplashScreen from "../../components/splash/SplashScreen";
 
 const AppEntry = () => {
-  const [splashDone, setSplashDone] = useState(false);
   const isLoggedIn = !!localStorage.getItem("access_token");
-
-  if (!splashDone) {
-    return <SplashScreen onFinish={() => setSplashDone(true)} />;
-  }
-
   return <Navigate to={isLoggedIn ? "/dashboard" : "/login"} replace />;
 };
 
