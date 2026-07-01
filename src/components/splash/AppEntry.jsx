9c4e9dd90a8ab4fb2e3f6f967037210167ffb515
sivap1015844 <sivap1015844@gmail.com>
2026-07-01 15:20:37 +0530

    create new login

--- src/components/splash/AppEntry.jsx
+++ src/components/splash/AppEntry.jsx
@@ -1,8 +1,15 @@
-// AppEntry.jsx
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
+import SplashScreen from "../../components/splash/SplashScreen";
 
 const AppEntry = () => {
+  const [splashDone, setSplashDone] = useState(false);
   const isLoggedIn = !!localStorage.getItem("access_token");
+
+  if (!splashDone) {
+    return <SplashScreen onFinish={() => setSplashDone(true)} />;
+  }
+
   return <Navigate to={isLoggedIn ? "/dashboard" : "/login"} replace />;
 };
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -80,6 +80,8 @@ import InvoiceView from "../pages/business_billing/InvoiceView";
 
 import PrivacyPolicy from "../pages/PrivacyPolicy";
 import SplashScreen from "../components/splash/SplashScreen";
+import  AppEntry from "../components/splash/AppEntry";
+import  AppLogin from "../pages/app/AppLogin";
 
 
 
@@ -88,6 +90,8 @@ const AppRoutes = () => {
     <Routes>
       {/* Public */}
       <Route path="/" element={<HomePage />} />
+          <Route path="/app-launch" element={<AppEntry />} />
+          <Route path="/login" element={<AppLogin />} />
           <Route path="/shop/:scannerId" element={<CustomerView />} />
           <Route path="/invoice/:invoiceId" element={ < InvoiceView/>}  />
 
