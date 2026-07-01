04e065f02668ee9ca1f29ddd92d042a3b99be0bf
sivap1015844 <sivap1015844@gmail.com>
2026-07-01 14:38:40 +0530

    force

--- src/components/splash/SplashScreen.jsx
+++ src/components/splash/SplashScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const TOTAL_DURATION = 4000;
+const TOTAL_DURATION = 3000;
 
 const SplashScreen = ({ onFinish }) => {
   const [mounted, setMounted] = useState(false);
